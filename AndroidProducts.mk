447c2f632a0f764c34e37ed1dc61ff872bb7949d
Mandoko <briancheruiyotkosilei@gmail.com>
2026-05-27 07:07:46 +0300

    initial core files

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_W4002.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_W4002-user \
+    twrp_W4002-userdebug \
+    twrp_W4002-eng
